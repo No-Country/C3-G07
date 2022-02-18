5ab00b1902ad2d4f347a05c926bae0de3c92874d
Mauricio Raul Ferreyra <mauriciotauro82@hotmail.com>
2022-02-18 17:47:09 -0300

    instalación mysql y varias dependencias más

---
+++ back/src/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const morgan = require('morgan')
+require('dotenv').config()
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+// initializations
+const app = express()
+
+//setting
+app.set('port', process.env.PORT || 8080)
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.engine('.hbs',exphbs.create({  
+    defaultLayout: 'main',
+    LayoutsDir:path.join(app.get('views'), 'layouts'),
+    partialsDir:path.join(app.get('views'),'partials'),
+    extname:'.hbs',
+    helpers:require('./lib/handlebars')
+}).engine)
+
+app.set('views engine', '.hbs')
+
+//middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Global Variables
+
+//Routes
+app.use(require('./routes'))
+app.use(require('./routes/authentication'))
+app.use('/links', require('./routes/links'))
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')))
+
+//starting the server
+app.listen(app.get('port'), () => {
+  console.log('Listeng on port', app.get('port'))
+})
