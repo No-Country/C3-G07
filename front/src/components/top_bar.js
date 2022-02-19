d30cb854796afdc4c3691ba588e022e702011991
joer9514 <joer9514@gmail.com>
2022-02-19 15:44:37 -0500

    [Update] components and added components home

--- front/src/components/top_bar.js
+++ front/src/components/top_bar.js
@@ -3,47 +3,11 @@ const d = document;
 /**
  * Top bar component
  * @param {btnToggle} string Hamburger menu selector
- * @returns {Node} Top bar component
+ * @returns {void}
  */
 function topBar(btnToggle) {
-  const $root = d.getElementById("root");
-  const $topBar = d.createElement("header");
-
-  $topBar.classList.add("header");
-
-  /* component */
-  $topBar.innerHTML = `
-  <div class="header__container">
-    <div class="header__toggle" id="toggle">
-        <i class="fas fa-bars"></i>
-    </div>
-
-    <nav class="menu">
-        <ul class="menu__list">
-            <li class="menu__item">
-                <a class="menu__link" href="#" rel="noopener noreferrer">Nosotros</a>
-            </li>
-            <li class="menu__item">
-                <a class="menu__link" href="#" rel="noopener noreferrer">Institucional</a>
-            </li>
-            <li class="menu__item">
-                <a class="menu__link" href="#" rel="noopener noreferrer">Especialidades</a>
-            </li>
-            <li class="menu__item">
-                <a class="menu__link" href="#" rel="noopener noreferrer">Contacto</a>
-            </li>
-        </ul>
-    </nav>
-
-    <a class="linked" href="index.html">
-        <img class="linked__logo" loading="lazy" src="./src/assets/images/Logo.png" alt="logo clínica pérez" />
-    </a>
-
-    <button class="btn" type="submit">Ingresar</button>
-  </div>  
-`;
-
   d.addEventListener("click", (e) => {
+    /* button toggle */
     if (e.target.matches(btnToggle) || e.target.matches(`${btnToggle} *`)) {
       d.querySelector(".menu").classList.toggle("menu--active");
       d.querySelector(".header__toggle i").classList.toggle("fa-times");
@@ -52,8 +16,6 @@ function topBar(btnToggle) {
       d.querySelector(".header__toggle i").classList.remove("fa-times");
     }
   });
-
-  return $root.appendChild($topBar);
 }
 
 export default topBar;
--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,10 @@
-import heroSection from "./components/hero.js";
-import main from "./components/main_layout.js";
-import emergency from "./components/emergency.js";
 import topBar from "./components/top_bar.js";
+import heroSection from "./components/hero.js";
 
 const d = document;
 
 /* render components */
 d.addEventListener("DOMContentLoaded", (e) => {
   topBar("#toggle");
-  main();
   heroSection();
-  emergency();
 });
