eba7a653c1d960c7dc5b69a5e03dedaca8e533cf
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2022-02-21 17:58:14 -0300

    falta conectar con el front

--- back/src/index.js
+++ back/src/index.js
@@ -7,28 +7,14 @@ const exphbs = require('express-handlebars')
 // initializations
 const app = express()
 
-//setting
-app.set('port', process.env.PORT || 8080)
-
-//app.set('views', path.join(__dirname, 'views'))
-
-// app.engine(
-//   '.hbs',
-//   exphbs.create({
-//     defaultLayout: 'main',
-//     LayoutsDir: path.join(app.get('views'), 'layouts'),
-//     partialsDir: path.join(app.get('views'), 'partials'),
-//     extname: '.hbs',
-//     helpers: require('./lib/handlebars')
-//   }).engine
-// )
-// app.set('views engine', '.hbs')
-
 //middleware
 app.use(morgan('dev'))
 app.use(express.static(__dirname + '/front'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Global Variables
 
@@ -36,9 +22,6 @@ app.use(express.json())
 app.use(require('./routes'))
 app.use(require('./routes/authentication'))
 app.use('/links', require('./routes/links'))
-
-//Public
-app.use(express.static(path.join(__dirname, 'public')))
 
 //starting the server
 app.listen(app.get('port'), () => {
