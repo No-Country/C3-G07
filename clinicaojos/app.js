10c5847460d2270a6289db892a9c783668f2cc95
Afalkenhagen <ahau_buluk@protonmail.ch>
2022-03-09 19:33:01 -0300

    feat: port for heroku deploy

--- clinicaojos/app.js
+++ clinicaojos/app.js
@@ -46,4 +46,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+
 module.exports = app;
