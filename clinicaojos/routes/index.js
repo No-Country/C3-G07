a9002e78b30ec1be99ac214896686bc80e53bd76
victordlt <v.delatorredeagit config --global user.email v.delatorredea@gmail.com>
2022-03-11 16:36:15 +0100

    Update index.js

--- clinicaojos/routes/index.js
+++ clinicaojos/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 const indexmodel=require('../Models/index_model');
 
+<<<<<<< Updated upstream
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+=======
+/* GET home page. .*/
+router.get('/',  (req, res, next) => {
+>>>>>>> Stashed changes
   const specialities =  indexmodel.getallspecialities();
   res.render('index', {especialidad:specialities});
 });
